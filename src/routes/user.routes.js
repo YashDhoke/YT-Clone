c5bbfc5af17cd165eb0854041cff31851940c99e
YashDhoke <yashdhoke1@gmail.com>
2024-07-02 15:31:29 +0530

    created logic for access and refresh token

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { logOutUser, loginUser, registerUser } from "../controllers/user.contoller.js";
+import { logOutUser, loginUser, registerUser  , refreshAccessToken } from "../controllers/user.contoller.js";
 import {upload} from '../middlewares/multer.middleware.js'
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -24,5 +24,6 @@ router.route("/login").post(loginUser) ;
 
 //secured routes 
 router.route("/logout").post(verifyJWT , logOutUser) ;
+router.route("/refresh-token").post(refreshAccessToken) ;
 
 export default router ; 
